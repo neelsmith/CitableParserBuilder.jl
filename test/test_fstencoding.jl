76d8ed01740fb18930861ddf712ed6e606f24205
Neel Smith <dnsmith.neel@gmail.com>
2021-03-23 08:35:52 -0400

    Catch underscores in URN values

--- test/test_fstencoding.jl
+++ test/test_fstencoding.jl
@@ -4,3 +4,9 @@
     fst = fstsafe(au)
     @test fst == "<u>lexicon\\.lex123</u>"
 end
+
+@testset "Test protecting underscores" begin
+    rule = RuleUrn("nouninfl.h_hs1")
+    fst = fstsafe(rule)
+    @test fst == "<u>nouninfl\\.h\\_hs1</u>"
+end
