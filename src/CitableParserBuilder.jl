94405c914ac86b87ae7554798b815ee126afe79b
Neel Smith <dnsmith.neel@gmail.com>
2021-08-04 09:49:29 -0400

    Add readfst function

--- src/CitableParserBuilder.jl
+++ src/CitableParserBuilder.jl
@@ -10,10 +10,12 @@ export AbbreviatedUrn
 export LexemeUrn, FormUrn, StemUrn, RuleUrn
 
 export fstsafe, expand, abbreviate, abbreviation
+export readfst
 
 include("abbrurn.jl")
 include("analysis.jl")
 include("citeurn.jl")
 include("types.jl")
+include("fstreader.jl")
 
 end # module
