de10879dc6eaf7f5f14abc71306b8d593d9f18f4
Neel Smith <dnsmith.neel@gmail.com>
2021-08-09 10:24:07 -0400

    Add comment on API contract.

--- src/parser.jl
+++ src/parser.jl
@@ -16,6 +16,8 @@ end
 
 
 $(SIGNATURES)
+
+Should return pairs of tokens with a (possibly empty) Vector of Analyses.
 """
 function parsewordlist(p::T, v) where {T <: CitableParser}
     @warn("parsewordlist function not defined for type ", typeof(p))
@@ -27,6 +29,8 @@ end
 """Required function to parse a list of tokens in a file with a `CitableParser`.
 
 $(SIGNATURES)
+
+Should return pairs of tokens with a (possibly empty) Vector of Analyses.
 """
 function parselistfromfile(p::T, f) where {T <: CitableParser}
     @warn("parselistfromfile function not defined for type ", typeof(p))
@@ -38,6 +42,8 @@ end
 """Required function to parse a list of tokens at a given url with a `CitableParser`.
 
 $(SIGNATURES)
+
+Should return pairs of tokens with a (possibly empty) Vector of Analyses.
 """
 function parselistfromurl(p::T, u) where {T <: CitableParser}
     @warn("parselistfromurl function not defined for type ", typeof(p))
