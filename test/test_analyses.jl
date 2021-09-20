a821a445fe66b2ed7cc26a045a292fea4c0b7483
Neel Smith <dnsmith.neel@gmail.com>
2021-09-20 06:21:48 -0400

    Inital work on Citable interface

--- test/test_analyses.jl
+++ test/test_analyses.jl
@@ -7,9 +7,9 @@
     stem = StemUrn("stems.example1")
     a = Analysis(str, lex, form, stem, rule)
     expected = "et,ls.n16278,morphforms.1000000001,rules.example1,stems.example1" 
-    @test CitableParserBuilder.cex(a) == expected
+    @test cex(a, ",") == expected
     piped  = "et|ls.n16278|morphforms.1000000001|rules.example1|stems.example1"
-    @test CitableParserBuilder.cex(a, "|") == piped
+    @test cex(a, "|") == piped
 end
 
 
@@ -22,24 +22,27 @@ end
     a = Analysis(str, lex, form, stem, rule)
 
     u = CtsUrn("urn:cts:demo:latin.sample:1")
-    cn = CitableNode(u, "Et")
+    cn = CitablePassage(u, "Et")
     tkn = AnalyzedToken(cn, [a]) 
-    expected = "Et|urn:cts:demo:latin.sample:1|et,ls.n16278,morphforms.1000000001,rules.example1,stems.example1"
-    @test CitableParserBuilder.cex(tkn) == expected
+    # Note that this 
+    expected = "et|urn:cts:demo:latin.sample:1|et|ls.n16278|morphforms.1000000001|rules.example1|stems.example1"
+    @test cex(tkn)[1] == expected
 end
 
 @testset "Test parsing serialized Analysis" begin
   cex = "οὑτω,lsj.n76063,morphforms.1000000004,litgreek.indeclinable4,uninflectedstems.n76063"
+  @test_broken "Serializing anlaysis" == nothing
+  #=
   a = fromcex(cex)
   @test isa(a, Analysis)
   @test a.lexeme == LexemeUrn("lsj.n76063")
   @test a.form == FormUrn("morphforms.1000000004")
   @test a.stem == StemUrn("litgreek.indeclinable4")
   @test a.rule == RuleUrn("uninflectedstems.n76063")
+  =#
 end
 
 
 @testset "Test parsing analyses from file" begin
-
-  
+  @test_broken 1 == 2
 end
