b0e3d33dd76a8499b57c121f915e096aaf29af04
Neel Smith <dnsmith.neel@gmail.com>
2024-06-07 13:54:43 -0400

    Start to rework sample parser

--- src/gettysburg.jl
+++ src/gettysburg.jl
@@ -5,7 +5,7 @@
 # POS values are taken from:
 # https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 #
-using CSV, HTTP
+using CSV, Downloads
 
 """POS tagger keyed to the text of the Gettysburg address. `data` is a dictionary of tokens to form POS tag.
 """
@@ -17,17 +17,47 @@ end
 
 GETTYSBURG_DICT_URL = "https://raw.githubusercontent.com/neelsmith/CitableCorpusAnalysis.jl/main/test/data/posdict.csv"
 
-"""Instantiate a `GettysburgParser`.
-"""
-function gettysburgParser(; dict = nothing) 
-    if isnothing(dict)
-        @info("Loading dictionary over the internet...")
-        downloaded = CSV.File(HTTP.get(CitableParserBuilder.GETTYSBURG_DICT_URL).body) |> Dict
-        @info("Done loading.")
-        GettysburgParser(downloaded)
-    else
-        GettysburgParser(dict)
+
+
+
+function gettsyburgDictToParser(dict::Dict; delimiter = ",")
+    hdr = join(["Token","Lexeme","Form","Stem","Rule"], delimiter)
+    
+    datarows = String[hdr]
+    for k in keys(dict)
+        row = string(k,delimiter, "gburglex.", k,delimiter, "pennpos.", dict[k],delimiter,"gburgstem.",k,delimiter, "gburgrule.pennid")
+        push!(datarows, row)
     end
+    csvsrc = join(datarows,"\n")
+    CSV.File(IOBuffer(csvsrc)) |> DataFrame
+end
+
+
+"""Instantiate a `GettysburgParser` as a dfParser by downloading source dictionary over the internet.
+"""
+function gettysburgParser() 
+   
+    downloaded = Downloads.download(CitableParserBuilder.GETTYSBURG_DICT_URL)
+
+    #downloaded = CSV.File(HTTP.get(CitableParserBuilder.GETTYSBURG_DICT_URL).body) |> Dict
+    @info("Done loading.")
+
+    dict = CSV.File(downloaded) |> Dict
+    rm(downloaded)
+    
+    gettsyburgDictToParser(dict; delimiter = delimiter)
+    
+end
+
+
+"""Instatiate `GettysburgParser` as a dfParser
+from a local file source.
+$(SIGNATURES)
+"""
+function gettysburgParser(repo::AbstractString; delimiter = ",")
+    src = joinpath(repo,"test","data","posdict.csv")
+    dict = CSV.File(src) |> Dict
+    gettsyburgDictToParser(dict; delimiter = delimiter)
 end
 
 function orthography(p::GettysburgParser)
